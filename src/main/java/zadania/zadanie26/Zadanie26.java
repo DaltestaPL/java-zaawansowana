d8bff7b0ae3a19fdf73ffb8a0a6095ec3a403bb8
Tomasz Wojtyra <t.wojtyra@il-pib.pl>
2022-11-09 21:02:11 +0100

    zadanie 29

--- src/main/java/zadania/zadanie26/Zadanie26.java
+++ src/main/java/zadania/zadanie26/Zadanie26.java
@@ -2,6 +2,7 @@ package zadania.zadanie26;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.function.Function;
 import java.util.stream.Collectors;
 
 /**
---
+++ src/main/java/zadania/zadanie29/Zadanie29.java
@@ -0,0 +1,21 @@
+package zadania.zadanie29;
+
+import java.util.function.Function;
+
+/**
+ * Zaimplementuj generyczną metodę partOf,
+ * która na podstawie tablicy dowolnego typu oraz wskazanej funkcji jako parametrów zwróci % elementów spełniających warunek.
+ */
+public class Zadanie29 {
+
+
+    public <P> double partOf(P[] array, Function<P, Boolean> function) {
+        function.apply()
+        return 0.0;
+    }
+
+
+    public static void main(String[] args) {
+
+    }
+}
